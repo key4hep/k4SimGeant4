2823a618439bf1955499f6d83d2cb9487b8eacf3
Giovanni Marchiori <39376142+giovannimarchiori@users.noreply.github.com>
2026-04-16 09:16:59 +0200

    Apply suggestion from @tmadlener
    Co-authored-by: Thomas Madlener <thomas.madlener@desy.de>

--- Detector/DetComponents/src/MergeLayers.cpp
+++ Detector/DetComponents/src/MergeLayers.cpp
@@ -62,7 +62,6 @@ StatusCode MergeLayers::initialize() {
             << endmsg;
     return StatusCode::FAILURE;
   }
-  auto cellIDEncodingName = podio::collMetadataParamName(m_outHits.objKey(), edm4hep::labels::CellIDEncoding);
   k4FWCore::putCellIDEncoding(m_outHits.objKey(), m_descriptor.fieldDescription(), this);
   info() << "Field description: " << m_descriptor.fieldDescription() << endmsg;
   info() << "Merging volumes named: " << m_volumeName << endmsg;
